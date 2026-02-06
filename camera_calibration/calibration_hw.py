8df40a2078e36496d8a10fbed04fbbe26f23f22e
Allan Howe <allandavidhowe@gmail.com>
2026-02-06 11:25:39 -0700

    focal length calc part 2

--- camera_calibration/calibration_hw.py
+++ camera_calibration/calibration_hw.py
@@ -1,21 +1,25 @@
 import cv2 as cv
 import os
+import numpy as np
 
 INPUT_IMG_FOLDER = "./calibration_images"
-OUTPUT_IMG_FOLDER = INPUT_IMG_FOLDER + "/output_images"
+OUTPUT_IMG_FOLDER = "./output_images"
 
-import cv2 as cv
-import os
 
-def task_1():
-    infile = os.path.join(INPUT_IMG_FOLDER, "AR1.jpg")
-    outfile = os.path.join(OUTPUT_IMG_FOLDER, "AR1.jpg")
-    os.mkdir(OUTPUT_IMG_FOLDER)
+pattern_size = (10, 7)  # inner corners (columns, rows)
+square_size = 1.0
+objp = np.zeros((pattern_size[0]*pattern_size[1], 3), np.float32)
+objp[:, :2] = np.mgrid[0:pattern_size[0], 0:pattern_size[1]].T.reshape(-1,2)
+objp *= square_size
+
+def task_1(file_name):
+    infile = os.path.join(INPUT_IMG_FOLDER, file_name)
+    outfile = os.path.join(OUTPUT_IMG_FOLDER, file_name)
+    os.makedirs(OUTPUT_IMG_FOLDER, exist_ok=True)
     image = cv.imread(infile)
     gray_image = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
     
     # Find chessboard corners
-    pattern_size = (10, 7)  # inner corners (columns, rows)
     ret, corners = cv.findChessboardCorners(gray_image, pattern_size)
     
     if ret:
@@ -31,24 +35,47 @@ def task_1():
         
         # Show the image
         cv.imwrite(outfile, gray_color)
+        return corners_refined
     else:
         print("Chessboard corners not found!")
 
 
 
 def task_2():
+    object_points = []
+    image_points = []
+
+    # get image size
+    first_image = cv.imread( os.path.join(INPUT_IMG_FOLDER, "AR1.jpg"))
+    h, w, c = first_image.shape
+    image_size = (w, h)
+    print(image_size)
+
     # Loop through all files in the folder
-    for filename in os.listdir(INPUT_IMG_FOLDER):
-        if not filename.lower().endswith(".jpg"):
-            continue  # skip non-image files
-
-        infile = os.path.join(INPUT_IMG_FOLDER, filename)
-        image = cv.imread(infile)
-
-
-
+    for file_name in os.listdir(INPUT_IMG_FOLDER):
+        if not file_name.lower().endswith(".jpg"):
+            continue
+        corners = task_1(file_name=file_name)
+        if corners is not None:
+            image_points.append(corners)
+            object_points.append(objp)
+    
+    ret, camera_matrix, dist, revecs, tvecs = cv.calibrateCamera(objectPoints=object_points, imagePoints=image_points, imageSize=image_size, cameraMatrix=None, distCoeffs=None)
+    
+    p_size_mm = 0.0074
+    if ret:
+        print("camera_matrix:", camera_matrix)
+        fx_p = camera_matrix[0,0]
+        fx_mm = fx_p * p_size_mm
+        fy_p = camera_matrix[1,1]
+        fy_mm = fy_p * p_size_mm
+        print("fx_p:", fx_p, "fy_p:", fy_p)
+        print("fx_mm:", fx_mm)
+        print("fy_mm:", fy_mm)
+        print("")
+        print("dist:", dist)
 
 
 if __name__ == "__main__":
 
-    task_1()
+    task_2()
