338af02dc8af628487ff96245d5b2c48e6e5d67e
Allan Howe <allandavidhowe@gmail.com>
2026-03-04 14:07:51 -0700

    removing unused fx

--- trajectory/4_trajectory.py
+++ trajectory/4_trajectory.py
@@ -2,33 +2,6 @@ import cv2
 import numpy as np
 import os
 
-# keep the start-end index images, remove the rest from the dataset 
-def keep_image_range(base_path, start, end):
-    for side in ["L", "R"]:
-        folder = os.path.join(base_path, side)
-
-        for file in os.listdir(folder):
-            if file.endswith(".png"):
-                idx = int(file.split(".")[0])
-
-                if idx < start or idx > end:
-                    os.remove(os.path.join(folder, file))
-
-
-def rename_files(base_path):
-    """
-    Rename all PNG files in L/ and R/ subfolders of base_path
-    to 0.png, 1.png, 2.png, ... based on sorted order.
-    """
-    for side in ["L", "R"]:
-        folder = os.path.join(base_path, side)
-        files = sorted(f for f in os.listdir(folder) if f.lower().endswith(".png"))
-
-        for idx, f in enumerate(files):
-            old_path = os.path.join(folder, f)
-            new_path = os.path.join(folder, f"{idx}.png")
-            os.rename(old_path, new_path)
-
 def read_and_crop_stereo(left_path, right_path, crop_box_l, crop_box_r):
     """
     Reads left and right images and returns cropped versions.
@@ -48,7 +21,6 @@ def read_and_crop_stereo(left_path, right_path, crop_box_l, crop_box_r):
     right_crop = right_img[y_start:y_end, x_start:x_end]
 
     return left_crop, right_crop
-
 
 def differencing(image, prev_ref_gray):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
